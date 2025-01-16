dc89ae8326a5f5ab5aa9c2269af125b8bc2e381b
Arun Maheshwari <maheshwariarun940@gmail.com>
2025-01-16 01:08:00 -0500

    Installed requirements and got google(gemini) api key

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name = "GenAI Project - Information Retrieval System",
+    version = "0.0.0",
+    author = "Arun Maheshwari",
+    author_email = "maheshwariarun940@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
